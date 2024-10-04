dd5d27d061ec0ba771327b1fcfc4607c6efc8193
andrei <freemail_2019@mail.ru>
2024-10-04 10:24:46 +0800

    v5.4

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 cd /home/user/gpu
 git pull origin main
 source env/bin/activate
-python manage.py collectstatic --no-input
-pip install -r requirements.txt
-python manage.py migrate
+python manage.py collectstatic
+#pip install -r requirements.txt
+#python manage.py migrate
 sudo systemctl restart ginicorn
