42ac6dd3a9afe0acd7c6e60ad85f7da59000f1cf
root <root@cv4159713.novalocal>
2024-10-23 10:42:20 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,6 @@ sudo git pull origin main
 source env/bin/activate
 python manage.py collectstatic --no-input
 pip install -r requirements.txt
+python manage.py makemigrations
 python manage.py migrate
-sudo systemctl restart ginicorn
+sudo systemctl restart gunicorn
