8d33f68c9100f7e6495963b188a73549eb078221
andrei <freemail_2019@mail.ru>
2024-11-22 10:31:45 +0800

    изменен стиль

--- defectone/views.py
+++ defectone/views.py
@@ -22,7 +22,7 @@ def defect_act(request, poz, n):
 def send_act(request, pk):
     de = Defect.objects.get(pk=pk)
     eq = Equipment.objects.filter(Q(serial_number=de.serial_number) & Q(model__name=de.model)).first()
-    wb = load_workbook(f'{BASE_DIR}/defect files/act1.xlsx')
+    wb = load_workbook(f'{BASE_DIR}/act1.xlsx')
     ws = wb['act']
     ws['E9'] = de.defect_act
     ws['J5'] = de.approve.name
@@ -44,7 +44,7 @@ def send_act(request, pk):
     ws['J41'] = de.kait.name
     ws['A43'] = de.worker.job_title
     ws['J44'] = de.worker.name
-    wb.save(f'{BASE_DIR}/defect files/{de.defect_act}.xlsx')
+    wb.save(f'{BASE_DIR}/files/{de.defect_act}.xlsx')
     wb.close()
     sm = EmailMessage
     subject = 'Worker'
@@ -52,7 +52,7 @@ def send_act(request, pk):
     from_email = 'freemail_2019@mail.ru'
     to_email = request.user.email
     msg = sm(subject, body, from_email, [to_email])
-    msg.attach_file(f'{BASE_DIR}/defect files/{de.defect_act}.xlsx')
+    msg.attach_file(f'{BASE_DIR}/files/{de.defect_act}.xlsx')
     msg.send()
     return redirect(reverse_lazy('defectone:defect_list'))
 
