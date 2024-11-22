7fb2ce419bee1aa3536e60dec75d9c8085ac9326
andrei <freemail_2019@mail.ru>
2024-11-22 16:07:18 +0800

    изменен стиль

--- device/sending.py
+++ device/sending.py
@@ -14,13 +14,13 @@ def sending(request, title):
     msg = sm(subject, body, from_email, [to_email])
     msg.attach_file(f'./{title}.csv')
     msg.send()
-    with open(f'./{title}.csv', 'w', encoding='utf-8'):
+    with open(f'./{title}.csv', 'w', encoding='Windows-1251'):
         pass
     return redirect(reverse_lazy('search'))
 
 
 def sample_send(request, data):
-    with open('./sample_send.csv', 'w', encoding='utf-8') as f:
+    with open('./sample_send.csv', 'w', encoding='Windows-1251') as f:
         fieldnames = ['№', 'position', 'location', 'teg', 'serial_number', 'type', 'model', 'name', 'reg_number',
                       'min_scale', 'max_scale', 'unit', 'comment', 'interval', 'previous_verification',
                       'next_verification', 'result', ]  #
@@ -69,13 +69,13 @@ def sample_send(request, data):
 
 def send_all(request, start, end):
     if start == 0:
-        with open('./all_data.csv', 'w', encoding='utf-8'):
+        with open('./all_data.csv', 'w', encoding='Windows-1251'):
             pass
     if not request.user.is_staff:
         redirect('login')
     last = Equipment.objects.all().count()
     get_all = Equipment.objects.filter(si_or=True)[start:end]
-    with open('./all_data.csv', 'a', encoding='utf-8') as f:
+    with open('./all_data.csv', 'a', encoding='Windows-1251') as f:
         fieldnames = ['№', 'position', 'location', 'teg', 'type', 'model', 'name', 'reg_number', 'serial_number', 'description',
                       'min_scale', 'max_scale', 'unit', 'comment', 'interval', 'previous_verification',
                       'next_verification', 'result', ]  #
