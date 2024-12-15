716d12fac5369e278755b76040e8ebcedfd3f140
andrei <freemail_2019@mail.ru>
2024-12-15 21:34:26 +0800

    изменен стиль

--- device/sending.py
+++ device/sending.py
@@ -18,7 +18,7 @@ def sending(request, title):
     msg = sm(subject, body, from_email, [to_email])
     msg.attach_file(f'./{title}.csv')
     msg.send()
-    with open(f'./{title}.csv', 'w', encoding='Windows-1251'):
+    with open(f'./{title}.csv', 'w', encoding='utf-8'):
         pass
     return redirect(reverse_lazy('search'))
 
@@ -38,26 +38,29 @@ def sample_send(request, data):
                     from_si = Si.objects.get(equipment=eq)
                 except:
                     continue
-                writer.writerow({
-                    '№': i + 1,
-                    'position': eq.positions.last().name,
-                    'location': eq.locations.last().name,
-                    'teg': eq.tags.last().name,
-                    # 'type': eq.type.name,
-                    'model': eq.model.name,
-                    'name': eq.name.name,
-                    'serial_number': eq.serial_number,
-                    'reg_number': from_si.reg_number,
-                    'min_scale': from_si.scale.min_scale,
-                    'max_scale': from_si.scale.max_scale,
-                    'unit': from_si.unit,
-                    'comment': from_si.com,
-                    'interval': from_si.interval,
-                    'previous_verification': from_si.previous_verification,
-                    'next_verification': from_si.next_verification,
-                    'result': from_si.result
-                }
-                )
+                try:
+                    writer.writerow({
+                        '№': i + 1,
+                        'position': eq.positions.last().name,
+                        'location': eq.locations.last().name,
+                        'teg': eq.tags.last().name,
+                        # 'type': eq.type.name,
+                        'model': eq.model.name,
+                        'name': eq.name.name,
+                        'serial_number': eq.serial_number,
+                        'reg_number': from_si.reg_number,
+                        'min_scale': from_si.scale.min_scale,
+                        'max_scale': from_si.scale.max_scale,
+                        'unit': from_si.unit,
+                        'comment': from_si.com,
+                        'interval': from_si.interval,
+                        'previous_verification': from_si.previous_verification,
+                        'next_verification': from_si.next_verification,
+                        'result': from_si.result
+                    }
+                    )
+                except:
+                    pass
             if not eq.si_or:
                 writer.writerow({
                     '№': i + 1,
@@ -75,15 +78,15 @@ def sample_send(request, data):
 
 def send_all(request, start, end):
     if start == 0:
-        with open('./all_data.csv', 'w', encoding='Windows-1251'):
+        with open('./all_data.csv', 'w', encoding='utf-8'):
             pass
     if not request.user.is_staff:
         redirect('login')
     last = Equipment.objects.all().count()
     get_all = Equipment.objects.filter(si_or=True)[start:end]
-    with open('./all_data.csv', 'a', encoding='Windows-1251') as f:
+    with open('./all_data.csv', 'a', encoding='utf-8') as f:
         fieldnames = ['№', 'position', 'location', 'teg',  'model', 'name', 'reg_number', 'serial_number', 'description',
-                      'min_scale', 'max_scale', 'unit', 'comment', 'interval', 'previous_verification',
+                      'min_scale', 'max_scale', 'comment', 'interval', 'previous_verification',
                       'next_verification', 'result', ]  #
         writer = csv.DictWriter(f, fieldnames=fieldnames, delimiter=';')
         writer.writeheader()
@@ -92,27 +95,30 @@ def send_all(request, start, end):
                 from_si = Si.objects.get(equipment=eq)
             except:
                 continue
-            writer.writerow({
-                '№': i + start,
-                'position': eq.positions.last().name,
-                'location': eq.locations.last().name,
-                'description': eq.descriptions.last().name,
-                'teg': eq.tags.last().name,
-                # 'type': eq.type.name,
-                'model': eq.model.name,
-                'name': eq.name.name,
-                'reg_number': from_si.reg_number,
-                'serial_number': eq.serial_number,
-                'min_scale': from_si.scale.min_scale,
-                'max_scale': from_si.scale.max_scale,
-                'unit': from_si.unit,
-                'comment': eq.comment,
-                'interval': from_si.interval,
-                'previous_verification': from_si.previous_verification,
-                'next_verification': from_si.next_verification,
-                'result': from_si.result,
-            }
-            )
+            try:
+                writer.writerow({
+                    '№': i + start,
+                    'position': eq.positions.last().name,
+                    'location': eq.locations.last().name,
+                    'description': eq.descriptions.last().name,
+                    'teg': eq.tags.last().name,
+                    # 'type': eq.type.name,
+                    'model': eq.model.name,
+                    'name': eq.name.name,
+                    'reg_number': from_si.reg_number,
+                    'serial_number': eq.serial_number,
+                    'min_scale': from_si.scale.min_scale,
+                    'max_scale': from_si.scale.max_scale,
+                    # 'unit': from_si.unit,
+                    'comment': eq.comment,
+                    'interval': from_si.interval,
+                    'previous_verification': from_si.previous_verification,
+                    'next_verification': from_si.next_verification,
+                    'result': from_si.result,
+                }
+                )
+            except:
+                pass
     if end < last:
         start = end
         end += 1000
