c16d107a98a743bad0dd49760836e66e04f42a09
andrei <freemail_2019@mail.ru>
2025-01-09 23:22:34 +0800

    обновление данных из аршина

--- equipment/urls.py
+++ equipment/urls.py
@@ -17,10 +17,18 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('', include('device.urls')),
     path('admin/', admin.site.urls),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+                  # ваши urls
+              ] + (static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
+
+urlpatterns += [re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+                re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, }), ]
