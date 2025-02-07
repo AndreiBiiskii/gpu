eaf6d8044cea857fddf9cb25d7914b7e7603dc25
andrei <freemail_2019@mail.ru>
2025-02-07 14:55:48 +0800

    парсинг

--- device/parser.py
+++ device/parser.py
@@ -34,100 +34,83 @@ def get_sample(table_tr):
 
 
 def data_from_parser(request):
-    eq = Equipment.objects.filter(serial_number='B0232596')
-    print(eq)
-    for j in eq:
-        for r in j.si.all():
-            print(j.year.name,
-                  j.model.name,
-                  j.year.name,
-                  r.interval.name,
-                  r.scale.min_scale,
-                  r.scale.max_scale,
-                  r.unit.name)
-        break
-        driver = webdriver.Chrome()
-        driver.get("https://fgis.gost.ru/fundmetrology/cm/results?rows=100&activeYear=%D0%92%D1%81%D0%B5")
-        data_ = driver.find_element(By.CLASS_NAME, 'modal-footer')
-        button = data_.find_element(By.TAG_NAME, 'button')
-        button.send_keys(Keys.ENTER)
-        wb_bag = load_workbook(f'{BASE_DIR}/bag.xlsx')
-        ws_bag = wb_bag['bag']
-        wb = load_workbook(f'{BASE_DIR}/from sending.xlsx')
-        ws = wb['l1']
-        wb_schema = load_workbook(f'{BASE_DIR}/schema.xlsm')
-        ws_schema = wb_schema['Лист1']
-        count = 0
-        try:
-            for i, eq in enumerate(ws):
-                count += 1
-                if count == 8:
+    driver = webdriver.Chrome()
+    driver.get("https://fgis.gost.ru/fundmetrology/cm/results?rows=100&activeYear=%D0%92%D1%81%D0%B5")
+    data_ = driver.find_element(By.CLASS_NAME, 'modal-footer')
+    button = data_.find_element(By.TAG_NAME, 'button')
+    button.send_keys(Keys.ENTER)
+    wb_bag = load_workbook(f'{BASE_DIR}/bag.xlsx')
+    ws_bag = wb_bag['bag']
+    wb = load_workbook(f'{BASE_DIR}/from sending.xlsx')
+    ws = wb['l1']
+    wb_schema = load_workbook(f'{BASE_DIR}/schema.xlsm')
+    ws_schema = wb_schema['Лист1']
+    count = 0
+    try:
+        for i, eq in enumerate(ws):
+            count += 1
+            if count == 8:
+                break
+            button = driver.find_elements(By.CLASS_NAME, 'btn')
+            button[1].send_keys(Keys.ENTER)
+            sleep(1)
+            name = driver.find_element(By.ID, 'filter_mi_mititle')
+            serial_number = driver.find_element(By.ID, 'filter_mi_number')
+            sleep(1)
+            serial_number.clear()
+            sleep(1)
+            serial_number.send_keys(ws[f'B{i + 2}'].value)
+            sleep(1)
+            name.clear()
+            sleep(1)
+            name.send_keys(ws[f'F{i + 2}'].value[0:4])
+            sleep(1)
+            btn = driver.find_elements(By.CLASS_NAME, 'btn-primary')
+            btn[0].send_keys(Keys.ENTER)
+            sleep(10)
+            table_div = driver.find_element(By.CLASS_NAME, 'sticky-spinner-wrap')
+            table_tr = table_div.find_elements(By.TAG_NAME, 'tr')
+            if len(table_tr) == 1:
+                ws_bag[f'A{i + 1}'] = ws[f'B{i + 2}'].value
+                ws_bag[f'B{i + 1}'] = ws[f'F{i + 2}'].value
+            if len(table_tr) > 1:
+                for con, row in enumerate(table_tr):
+                    if con == 0:
+                        continue
+                    table_td = row.find_elements(By.TAG_NAME, 'td')
+                    if table_td[5].text != ws[f'B{i + 2}'].value:
+                        continue
+                    print(table_td[5].text, ws[f'B{i + 2}'].value)
+                    ws_schema[f'J{i + 2}'] = ws[f'H{i + 2}'].value
+                    ws_schema[f'AP{i + 2}'] = ws[f'L{i + 2}'].value
+                    ws_schema[f'AK{i + 2}'] = ws[f'L{i + 2}'].value
+                    ws_schema[f'AU{i + 2}'] = ws[f'L{i + 2}'].value
+                    ws_schema[f'L{i + 2}'] = ws[f'J{i + 2}'].value
+                    ws_schema[f'O{i + 2}'] = ws[f'K{i + 2}'].value
+                    ws_schema[f'N{i + 2}'] = ws[f'M{i + 2}'].value
+                    ws_schema[f'Q{i + 2}'] = ws[f'M{i + 2}'].value
+                    ws_schema[f'A{i + 2}'] = table_td[3].text + '  ' + table_td[4].text
+                    ws_schema[f'B{i + 2}'] = table_td[2].text
+                    ws_schema[f'C{i + 2}'] = table_td[1].text
+                    ws_schema[f'I{i + 2}'] = table_td[5].text
+                    ws_schema[f'AR{i + 2}'] = table_td[6].text
+                    ws_schema[f'AS{i + 2}'] = table_td[6].text
+                    ws_schema[f'AY{i + 2}'] = table_td[8].text
+                    ws_schema[f'AG{i + 2}'] = table_td[0].text
                     break
-                button = driver.find_elements(By.CLASS_NAME, 'btn')
-                button[1].send_keys(Keys.ENTER)
-                sleep(1)
-                name = driver.find_element(By.ID, 'filter_mi_mititle')
-                serial_number = driver.find_element(By.ID, 'filter_mi_number')
-                sleep(1)
-                serial_number.clear()
-                sleep(1)
-                serial_number.send_keys(ws[f'B{i + 2}'].value)
-                sleep(1)
-                name.clear()
-                sleep(1)
-                name.send_keys(ws[f'F{i + 2}'].value[0:4])
-                sleep(1)
-                btn = driver.find_elements(By.CLASS_NAME, 'btn-primary')
-                btn[0].send_keys(Keys.ENTER)
-                sleep(10)
-                table_div = driver.find_element(By.CLASS_NAME, 'sticky-spinner-wrap')
-                table_tr = table_div.find_elements(By.TAG_NAME, 'tr')
-                if len(table_tr) == 1:
-                    ws_bag[f'A{i +1}'] = ws[f'B{i + 2}'].value
-                    ws_bag[f'B{i + 1}'] = ws[f'F{i + 2}'].value
-                if len(table_tr) > 1:
-                    for con, row in enumerate(table_tr):
-                        if con == 0:
-                            continue
-                        table_td = row.find_elements(By.TAG_NAME, 'td')
-                        if table_td[5].text != ws[f'B{i + 2}'].value:
-                            continue
-                        print(table_td[5].text, ws[f'B{i + 2}'].value)
-                        eq = Equipment.objects.filter(
-                            Q(serial_number=ws[f'B{i + 2}'].value) & Q(
-                                model__name=ws[f'G{i + 2}'].value))
-                        for j in eq:
-                            for r in j.si.all():
-                                ws_schema[f'J{i + 2}'] = j.year.name
-                                ws_schema[f'AP{i + 2}'] = r.interval.name
-                                ws_schema[f'AK{i + 2}'] = r.interval.name
-                                ws_schema[f'AU{i + 2}'] = r.interval.name
-                                ws_schema[f'L{i + 2}'] = r.scale.min_scale
-                                ws_schema[f'O{i + 2}'] = r.scale.max_scale
-                                ws_schema[f'N{i + 2}'] = r.unit.name
-                                ws_schema[f'Q{i + 2}'] = r.unit.name
-                        ws_schema[f'A{i + 2}'] = table_td[3].text + '  ' + table_td[4].text
-                        ws_schema[f'B{i + 2}'] = table_td[2].text
-                        ws_schema[f'C{i + 2}'] = table_td[1].text
-                        ws_schema[f'I{i + 2}'] = table_td[5].text
-                        ws_schema[f'AR{i + 2}'] = table_td[6].text
-                        ws_schema[f'AS{i + 2}'] = table_td[6].text
-                        ws_schema[f'AY{i + 2}'] = table_td[8].text
-                        ws_schema[f'AG{i + 2}'] = table_td[0].text
-                        break
-                print(f'Проверено {i} из 1194. Осталось {1194 - i} {(i * 100) / 1197}%')
-        finally:
-            wb_schema.save(f'{BASE_DIR}/from_schema.xlsx')
-            wb_schema.close()
-            wb_bag.save(f'{BASE_DIR}/from_bag.xlsx')
-            wb_bag.close()
-            sm = EmailMessage
-            subject = 'Schema'
-            body = 'Выборка отправлена на почту.'
-            from_email = 'freemail_2019@mail.ru'
-            to_email = request.user.email
-            msg = sm(subject, body, from_email, [to_email])
-            msg.attach_file(f'{BASE_DIR}/from_schema.xlsx')
-            msg.send()
+            print(f'Проверено {i} из 1194. Осталось {1194 - i} {(i * 100) / 1197}%')
+    finally:
+        wb_schema.save(f'{BASE_DIR}/from_schema.xlsx')
+        wb_schema.close()
+        wb_bag.save(f'{BASE_DIR}/from_bag.xlsx')
+        wb_bag.close()
+        sm = EmailMessage
+        subject = 'Schema'
+        body = 'Выборка отправлена на почту.'
+        from_email = 'freemail_2019@mail.ru'
+        to_email = request.user.email
+        msg = sm(subject, body, from_email, [to_email])
+        msg.attach_file(f'{BASE_DIR}/from_schema.xlsx')
+        msg.send()
 
     return redirect('/')
