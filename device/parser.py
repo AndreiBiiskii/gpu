8ada1aa3ab1b66fae91085749571cdae9fb5cc59
andrei <freemail_2019@mail.ru>
2025-02-22 19:47:07 +0800

    парсинг

--- device/parser.py
+++ device/parser.py
@@ -43,9 +43,9 @@ def data_from_parser(request):
     chrome_options = Options()
     # chrome_options.add_argument(r"-user-data-dir=/home/andrei/Desktop/pro/.venv/lib/python3.10/site-packages")
     chrome_options.add_argument(r"--user-data-dir=/home/user/gpu/env/lib/python3.10/site-packages/Prof1")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-gpu")
+    # chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--no-sandbox")
+    # chrome_options.add_argument("--disable-gpu")
 
     driver = webdriver.Chrome(options=chrome_options)
     # Version 133.0.6943.126 (Official Build) (64-bit)
