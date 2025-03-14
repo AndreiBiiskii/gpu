0d5e053daed02e446e4398f099582bd7bf72c3d8
andrei <freemail_2019@mail.ru>
2025-03-14 22:27:49 +0800

    парсинг

--- device/parser.py
+++ device/parser.py
@@ -50,7 +50,7 @@ def data_from_parser(request):
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-gpu")
 
-    driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()), options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options)
     driver.get("https://fgis.gost.ru/fundmetrology/cm/results?rows=100&activeYear=%D0%92%D1%81%D0%B5")
     data_ = driver.find_element(By.CLASS_NAME, 'modal-footer')
     button = data_.find_element(By.TAG_NAME, 'button')
