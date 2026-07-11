6ddcae77fb5004bd0c68f6c71294539938fa85e4
andrei <freemail_2019@mail.ru>
2026-07-11 23:53:52 +0800

    forms

--- equipment/settings.py
+++ equipment/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = True
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['95.163.220.164','127.0.0.1']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
 # Application definition
 
 INSTALLED_APPS = [
